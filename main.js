df36d31c48de7a204e7b4b70ba7fced025212d8f
Muhammad Hamdan Bhatti <hamdanbutt991gmail.com>
2024-04-24 03:02:20 +0500

    New Changes

--- main.js
+++ main.js
@@ -4,39 +4,39 @@ import chalk from "chalk";
 let runAgain = true;
 while (runAgain) {
     const choice = await inquirer.prompt({
-        name: 'option',
-        type: 'list',
-        choices: ['Count Words', 'Count Letters'],
+        name: "option",
+        type: "list",
+        choices: ["Count Words", "Count Letters"],
         message: "What do you want to do?",
     });
-    if (choice.option === 'Count Words') {
+    if (choice.option === "Count Words") {
         const userInput = await inquirer.prompt({
-            name: 'paragraph',
-            type: 'input',
-            message: 'Enter a paragraph to count words',
+            name: "paragraph",
+            type: "input",
+            message: "Enter a paragraph to count words",
         });
-        const wordCount = userInput.paragraph.split(' ').length;
-        console.log(chalk.blue('Total Number of Words:'));
-        console.log(chalk.redBright(wordCount));
+        const wordCount = userInput.paragraph.split(" ").length;
+        console.log(chalk.greenBright("============ Total Number of Words:"));
+        console.log(chalk.magentaBright(`======= >> ${wordCount} << ========`));
     }
     else {
         const userInput = await inquirer.prompt({
-            name: 'paragraph',
-            type: 'input',
-            message: 'Enter a paragraph to count letters',
+            name: "paragraph",
+            type: "input",
+            message: "Enter a paragraph to count letters",
         });
-        const letterCount = userInput.paragraph.replace(/\s/g, '').length;
-        console.log(chalk.blue('Total Number of Letters:'));
-        console.log(chalk.redBright(letterCount));
+        const letterCount = userInput.paragraph.replace(/\s/g, "").length;
+        console.log(chalk.green("============= Total Number of Letters:"));
+        console.log(chalk.magentaBright(`======== >> ${letterCount} << ========`));
     }
     const repeat = await inquirer.prompt({
-        name: 'again',
-        type: 'confirm',
-        message: 'Would you like to count again?',
+        name: "again",
+        type: "confirm",
+        message: "Would you like to count again?",
     });
     if (repeat.again === false) {
         runAgain = false;
-        console.log(chalk.blueBright(`${chalk.redBright("=".repeat(45))}\n"Thank you for using this application!"`));
+        console.log(chalk.yellow(`${chalk.redBright("=".repeat(45))}\n"Thank you for using this application!"`));
         console.log(chalk.green("This Program Created By: Muhammad Hamdan Bhatti"));
     }
 }
--- main.ts
+++ main.ts
@@ -5,52 +5,54 @@ import chalk from "chalk";
 let runAgain = true;
 
 while (runAgain) {
-
-  const choice = await inquirer.prompt({
-    name: 'option',
-    type: 'list',
-    choices: ['Count Words', 'Count Letters'],
+   const choice = await inquirer.prompt({
+      name: "option",
+      type: "list",
+      choices: ["Count Words", "Count Letters"],
       message: "What do you want to do?",
-  });
+   });
 
-  if (choice.option === 'Count Words') {
+   if (choice.option === "Count Words") {
+      const userInput = await inquirer.prompt({
+         name: "paragraph",
+         type: "input",
+         message: "Enter a paragraph to count words",
+      });
 
-    const userInput = await inquirer.prompt({
-      name: 'paragraph',
-      type: 'input',
-      message: 'Enter a paragraph to count words',
-    });
+      const wordCount = userInput.paragraph.split(" ").length;
 
-    const wordCount = userInput.paragraph.split(' ').length;
+      console.log(chalk.greenBright("============ Total Number of Words:"));
+      console.log(chalk.magentaBright(`======= >> ${wordCount} << ========`));
+   } else {
+      const userInput = await inquirer.prompt({
+         name: "paragraph",
+         type: "input",
+         message: "Enter a paragraph to count letters",
+      });
 
-    console.log(chalk.blue('Total Number of Words:'));
-    console.log(chalk.redBright(wordCount));
+      const letterCount = userInput.paragraph.replace(/\s/g, "").length;
 
-  } else {
+      console.log(chalk.green("============= Total Number of Letters:"));
+      console.log(chalk.magentaBright(`======== >> ${letterCount} << ========`));
+   }
 
-    const userInput = await inquirer.prompt({
-      name: 'paragraph',
-      type: 'input',
-      message: 'Enter a paragraph to count letters',
-    });
+   const repeat = await inquirer.prompt({
+      name: "again",
+      type: "confirm",
+      message: "Would you like to count again?",
+   });
 
-    const letterCount = userInput.paragraph.replace(/\s/g, '').length;
-
-    console.log(chalk.blue('Total Number of Letters:'));
-    console.log(chalk.redBright(letterCount));
-
-  }
-
-  const repeat = await inquirer.prompt({
-    name: 'again',
-    type: 'confirm',
-    message: 'Would you like to count again?',
-  });
-
-  if(repeat.again === false){
+   if (repeat.again === false) {
       runAgain = false;
-      console.log(chalk.blueBright(`${chalk.redBright("=".repeat(45))}\n"Thank you for using this application!"`));
-      console.log(chalk.green("This Program Created By: Muhammad Hamdan Bhatti"));
-  }
-
+      console.log(
+         chalk.yellow(
+            `${chalk.redBright(
+               "=".repeat(45)
+            )}\n"Thank you for using this application!"`
+         )
+      );
+      console.log(
+         chalk.green("This Program Created By: Muhammad Hamdan Bhatti")
+      );
+   }
 }
